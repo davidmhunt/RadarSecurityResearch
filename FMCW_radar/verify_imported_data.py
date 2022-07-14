068114a4cd47aacb57d428b4976841ec2ab17156
davidmhunt3 <davidmhunt3@gmail.com>
2022-07-14 17:13:09 -0400

    verified chirp and conversion samples

---
+++ FMCW_radar/verify_imported_data.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#dtype = np.complex64
+dtype = np.complex64
+
+try:
+    #with open("home/david/Documents/RadarSecurityResearch/FMCW_radar/sample_chirps/chirp_full.bin") as f:
+    with open("/Users/David/Documents/RadarSecurityResearch/FMCW_radar/sample_chirps/chirp_full.bin") as f:
+        numpy_data = np.fromfile(f,dtype)
+        size = numpy_data.size
+    print(numpy_data[533])
+    print(size)
+except IOError:
+    print("Error While Opening the file")
