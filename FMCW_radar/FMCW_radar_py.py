32d15add7512e61535fd4080c3011a9860e3e9f7
David <david.hunt@duke.edu>
2022-07-14 18:41:57 -0400

    preliminary flow graph

--- FMCW_radar/FMCW_radar_py.py
+++ FMCW_radar/FMCW_radar_py.py
@@ -20,20 +20,23 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
 from gnuradio import blocks
 import pmt
+from gnuradio import fft
+from gnuradio.fft import window
 from gnuradio import filter
-from gnuradio.filter import firdes
 from gnuradio import gr
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import uhd
-import time
-from gnuradio import zeromq
+from gnuradio.qtgui import Range, RangeWidget
 
 from gnuradio import qtgui
 
@@ -73,61 +76,144 @@ class FMCW_radar_py(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
-        self.decimation_factor = decimation_factor = 10
-        self.chirp_length = chirp_length = 10
+        self.samp_rate = samp_rate = 28.8e6
+        self.target_distance = target_distance = 1
+        self.noise_level = noise_level = 0
+        self.decimator_taps = decimator_taps = firdes.low_pass(1.0, samp_rate, 5e6,10e6, firdes.WIN_HAMMING, 6.76)
+        self.decimation_factor = decimation_factor = 9
+        self.chirp_length = chirp_length = 64
 
         ##################################################
         # Blocks
         ##################################################
-        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_gr_complex, chirp_length, '', 100, False, -1)
-        self.uhd_usrp_source_0 = uhd.usrp_source(
-            ",".join(("", "")),
-            uhd.stream_args(
-                cpu_format="fc32",
-                args='',
-                channels=list(range(0,1)),
-            ),
+        self._target_distance_range = Range(0, 479.67, 7.49, 1, 200)
+        self._target_distance_win = RangeWidget(self._target_distance_range, self.set_target_distance, 'target_distance', "counter_slider", float)
+        self.top_layout.addWidget(self._target_distance_win)
+        self._noise_level_range = Range(0, 0.01, 0.001, 0, 200)
+        self._noise_level_win = RangeWidget(self._noise_level_range, self.set_noise_level, 'noise_level', "counter_slider", float)
+        self.top_layout.addWidget(self._noise_level_win)
+        self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
+            chirp_length,
+            0,
+            7.49,
+            "range",
+            "magnitude",
+            "",
+            1 # Number of inputs
         )
-        self.uhd_usrp_source_0.set_center_freq(0, 0)
-        self.uhd_usrp_source_0.set_gain(0, 0)
-        self.uhd_usrp_source_0.set_antenna('RX2', 0)
-        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
-            ",".join(("", "")),
-            uhd.stream_args(
-                cpu_format="fc32",
-                args='',
-                channels=list(range(0,1)),
-            ),
-            '',
+        self.qtgui_vector_sink_f_0.set_update_time(0.10)
+        self.qtgui_vector_sink_f_0.set_y_axis(-140, 10)
+        self.qtgui_vector_sink_f_0.enable_autoscale(True)
+        self.qtgui_vector_sink_f_0.enable_grid(False)
+        self.qtgui_vector_sink_f_0.set_x_axis_units("m")
+        self.qtgui_vector_sink_f_0.set_y_axis_units("dB")
+        self.qtgui_vector_sink_f_0.set_ref_level(0)
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_vector_sink_f_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_vector_sink_f_0.set_line_label(i, labels[i])
+            self.qtgui_vector_sink_f_0.set_line_width(i, widths[i])
+            self.qtgui_vector_sink_f_0.set_line_color(i, colors[i])
+            self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_vector_sink_f_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+            chirp_length, #size
+            samp_rate/decimation_factor, #samp_rate
+            "", #name
+            1 #number of inputs
         )
-        self.uhd_usrp_sink_0.set_center_freq(0, 0)
-        self.uhd_usrp_sink_0.set_gain(0, 0)
-        self.uhd_usrp_sink_0.set_antenna('TX/RX', 0)
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec())
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=decimation_factor,
-                taps=None,
-                fractional_bw=0)
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                if (i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.fir_filter_xxx_0 = filter.fir_filter_ccc(decimation_factor, decimator_taps)
+        self.fir_filter_xxx_0.declare_sample_delay(0)
+        self.fft_vxx_0 = fft.fft_vcc(chirp_length, True, window.hanning(chirp_length), False, 1)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, chirp_length)
+        self.blocks_nlog10_ff_0 = blocks.nlog10_ff(10, chirp_length, 0)
+        self.blocks_multiply_const_xx_0 = blocks.multiply_const_cc(1/chirp_length, chirp_length)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(0.01)
         self.blocks_multiply_conjugate_cc_0 = blocks.multiply_conjugate_cc(1)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/david/Documents/RadarSecurityResearch/FMCW_radar/sample_chirps/chirp_samplePeriod.bin', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, int(samp_rate * 2 * target_distance / (3e8)))
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(chirp_length)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise_level, 0)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_nlog10_ff_0, 0))
+        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_delay_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_multiply_conjugate_cc_0, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.uhd_usrp_sink_0, 0))
-        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.zeromq_pub_sink_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_conjugate_cc_0, 1))
+        self.connect((self.blocks_multiply_conjugate_cc_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_multiply_const_xx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+        self.connect((self.blocks_nlog10_ff_0, 0), (self.qtgui_vector_sink_f_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.fir_filter_xxx_0, 0))
+        self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_const_xx_0, 0))
+        self.connect((self.fir_filter_xxx_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.fir_filter_xxx_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
@@ -140,20 +226,45 @@ class FMCW_radar_py(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.set_decimator_taps(firdes.low_pass(1.0, self.samp_rate, 5e6, 10e6, firdes.WIN_HAMMING, 6.76))
+        self.blocks_delay_0.set_dly(int(self.samp_rate * 2 * self.target_distance / (3e8)))
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimation_factor)
+
+    def get_target_distance(self):
+        return self.target_distance
+
+    def set_target_distance(self, target_distance):
+        self.target_distance = target_distance
+        self.blocks_delay_0.set_dly(int(self.samp_rate * 2 * self.target_distance / (3e8)))
+
+    def get_noise_level(self):
+        return self.noise_level
+
+    def set_noise_level(self, noise_level):
+        self.noise_level = noise_level
+        self.analog_noise_source_x_0.set_amplitude(self.noise_level)
+
+    def get_decimator_taps(self):
+        return self.decimator_taps
+
+    def set_decimator_taps(self, decimator_taps):
+        self.decimator_taps = decimator_taps
+        self.fir_filter_xxx_0.set_taps(self.decimator_taps)
 
     def get_decimation_factor(self):
         return self.decimation_factor
 
     def set_decimation_factor(self, decimation_factor):
         self.decimation_factor = decimation_factor
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/self.decimation_factor)
 
     def get_chirp_length(self):
         return self.chirp_length
 
     def set_chirp_length(self, chirp_length):
         self.chirp_length = chirp_length
+        self.blocks_multiply_const_xx_0.set_k(1/self.chirp_length)
 
 
 
